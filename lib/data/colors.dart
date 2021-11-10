e16d4ea9dd63e5de63a1c6e0997bb05ba09c6ad6
yaraoprysk <57788210+yaraoprysk@users.noreply.github.com>
2021-11-10 23:37:33 +0200

    Added data for search view

--- lib/data/colors.dart
+++ lib/data/colors.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color lightBlue = Color.fromRGBO(0, 149, 246, 1);
+const double defaultMargin = 14;
 
-const Color white = Colors.white;
-const Color black = Colors.black;
-
-Color grey = black.withOpacity(0.4);
-Color lightGrey = grey.withOpacity(0.15);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color greyColor = const Color(0xFFFAFAFA);
+Color greyDarkColor = const Color(0xFF8E8E8E);
