db07580bb12c4dd770b42c2e5c98df4a83fb5402
Viktoriia Yehorova <egorova.vik@gmail.com>
2021-11-11 02:09:30 +0200

    Provided global state for user profile tabs and photos with Redux

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
-import 'package:instagram_app/widgets/user_profile_widgets/profile_app_bar.dart';
-import 'package:instagram_app/widgets/user_profile_widgets/profile_body.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/app_bar.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/control_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/dashboard_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/info_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/tabs_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/highlights_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/sections/gallery_section.dart';
+import 'package:instagram_app/widgets/user_profile_widgets/separator.dart';
 
 class UserProfilePage extends StatefulWidget {
   const UserProfilePage({Key? key}) : super(key: key);
@@ -14,10 +19,30 @@ class UserProfilePage extends StatefulWidget {
 class _UserProfilePageState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: UserProfileHeader(),
-      body: UserProfileBody(),
+      body: ListView(
+        children: [
+          const HorizontalSeparator(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                DashboardSection(),
+                HorizontalSeparator(),
+                UserInfoSection(),
+                ControlSection(),
+              ],
+            ),
+          ),
+          const HighlightsSection(),
+          const TabsSection(),
+          const HorizontalSeparator(),
+          const GallerySection(),
+        ],
+      ),
     );
   }
 }
